d6bf4c24db1ce2e8b9117f33735b849802b80bf9
karthigenie <108732577+karthigenie@users.noreply.github.com>
2023-03-21 10:43:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CICD test 4
+          Learn React CICD test 5
         </a>
       </header>
     </div>
